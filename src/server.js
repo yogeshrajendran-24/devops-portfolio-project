d96a73dd04627f7d630ee1bcad78a6374aef4148
yogeshrajendran-24 <yogeshrajendran2410@gmail.com>
2025-12-10 12:59:40 +0530

    files updated

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello DevOps Project!");
+  res.send("This is my portfolio!");
 });
 
 app.get("/api/status", (req, res) => {
--- tests/api.test.js
+++ tests/api.test.js
@@ -3,5 +3,5 @@ const app = require("../src/server");
 
 it("GET / → returns message", async () => {
   const res = await request(app).get("/");
-  expect(res.text).toBe("Hello DevOps Project!");
+  expect(res.text).toBe("This is my portfolio!");
 });
